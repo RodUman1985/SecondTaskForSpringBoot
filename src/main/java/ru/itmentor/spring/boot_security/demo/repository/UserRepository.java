4461eb5eecf9a6889e18118c899f44435e6b8546
RodUman1985 <Vand_85@mail.ru>
2025-06-16 00:02:26 +0300

    11 вариант исправлений

--- src/main/java/ru/itmentor/spring/boot_security/demo/repository/UserRepository.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/repository/UserRepository.java
@@ -11,8 +11,7 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email = :email")
-    Optional<User> findByEmail(String email);
+
 
     @Query("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email = :email")
     Optional<User> findByEmailWithRoles(@Param("email") String email);
--- src/main/java/ru/itmentor/spring/boot_security/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/ru/itmentor/spring/boot_security/demo/service/UserDetailsServiceImpl.java
@@ -15,7 +15,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email)
+        return userRepository.findByEmailWithRoles(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
     }
 }
